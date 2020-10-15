72132b34329fa93ec4c9024674e29ceeb1d35d6a
kaleb <kalebclement19@gmail.com>
2020-10-15 14:28:45 +0700

    new

---
+++ 3D/Form1.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GlControl1 = New OpenTK.GLControl()
+        Me.RightBtn = New System.Windows.Forms.Button()
+        Me.LeftBtn = New System.Windows.Forms.Button()
+        Me.ForwardBtn = New System.Windows.Forms.Button()
+        Me.BackwardBtn = New System.Windows.Forms.Button()
+        Me.RotateYPlusBtn = New System.Windows.Forms.Button()
+        Me.RotateYMinBtn = New System.Windows.Forms.Button()
+        Me.UpBtn = New System.Windows.Forms.Button()
+        Me.DownBtn = New System.Windows.Forms.Button()
+        Me.RotateXMinBtn = New System.Windows.Forms.Button()
+        Me.RotateXPlusBtn = New System.Windows.Forms.Button()
+        Me.BackflipBtn = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.FrontflipBtn = New System.Windows.Forms.Button()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'GlControl1
+        '
+        Me.GlControl1.BackColor = System.Drawing.Color.Black
+        Me.GlControl1.Location = New System.Drawing.Point(249, 45)
+        Me.GlControl1.Name = "GlControl1"
+        Me.GlControl1.Size = New System.Drawing.Size(500, 500)
+        Me.GlControl1.TabIndex = 0
+        Me.GlControl1.VSync = False
+        '
+        'RightBtn
+        '
+        Me.RightBtn.Location = New System.Drawing.Point(775, 348)
+        Me.RightBtn.Name = "RightBtn"
+        Me.RightBtn.Size = New System.Drawing.Size(75, 23)
+        Me.RightBtn.TabIndex = 3
+        Me.RightBtn.Text = "Right"
+        Me.RightBtn.UseVisualStyleBackColor = True
+        '
+        'LeftBtn
+        '
+        Me.LeftBtn.Location = New System.Drawing.Point(139, 348)
+        Me.LeftBtn.Name = "LeftBtn"
+        Me.LeftBtn.Size = New System.Drawing.Size(75, 23)
+        Me.LeftBtn.TabIndex = 4
+        Me.LeftBtn.Text = "Left"
+        Me.LeftBtn.UseVisualStyleBackColor = True
+        '
+        'ForwardBtn
+        '
+        Me.ForwardBtn.Location = New System.Drawing.Point(457, 16)
+        Me.ForwardBtn.Name = "ForwardBtn"
+        Me.ForwardBtn.Size = New System.Drawing.Size(75, 23)
+        Me.ForwardBtn.TabIndex = 5
+        Me.ForwardBtn.Text = "Forward"
+        Me.ForwardBtn.UseVisualStyleBackColor = True
+        '
+        'BackwardBtn
+        '
+        Me.BackwardBtn.Location = New System.Drawing.Point(457, 551)
+        Me.BackwardBtn.Name = "BackwardBtn"
+        Me.BackwardBtn.Size = New System.Drawing.Size(75, 23)
+        Me.BackwardBtn.TabIndex = 6
+        Me.BackwardBtn.Text = "Backward"
+        Me.BackwardBtn.UseVisualStyleBackColor = True
+        '
+        'RotateYPlusBtn
+        '
+        Me.RotateYPlusBtn.Location = New System.Drawing.Point(775, 119)
+        Me.RotateYPlusBtn.Name = "RotateYPlusBtn"
+        Me.RotateYPlusBtn.Size = New System.Drawing.Size(75, 23)
+        Me.RotateYPlusBtn.TabIndex = 7
+        Me.RotateYPlusBtn.Text = "Rotate Y+"
+        Me.RotateYPlusBtn.UseVisualStyleBackColor = True
+        '
+        'RotateYMinBtn
+        '
+        Me.RotateYMinBtn.Location = New System.Drawing.Point(139, 119)
+        Me.RotateYMinBtn.Name = "RotateYMinBtn"
+        Me.RotateYMinBtn.Size = New System.Drawing.Size(75, 23)
+        Me.RotateYMinBtn.TabIndex = 8
+        Me.RotateYMinBtn.Text = "Rotate Y-"
+        Me.RotateYMinBtn.UseVisualStyleBackColor = True
+        '
+        'UpBtn
+        '
+        Me.UpBtn.Location = New System.Drawing.Point(303, 16)
+        Me.UpBtn.Name = "UpBtn"
+        Me.UpBtn.Size = New System.Drawing.Size(75, 23)
+        Me.UpBtn.TabIndex = 9
+        Me.UpBtn.Text = "Up"
+        Me.UpBtn.UseVisualStyleBackColor = True
+        '
+        'DownBtn
+        '
+        Me.DownBtn.Location = New System.Drawing.Point(303, 551)
+        Me.DownBtn.Name = "DownBtn"
+        Me.DownBtn.Size = New System.Drawing.Size(75, 23)
+        Me.DownBtn.TabIndex = 10
+        Me.DownBtn.Text = "Down"
+        Me.DownBtn.UseVisualStyleBackColor = True
+        '
+        'RotateXMinBtn
+        '
+        Me.RotateXMinBtn.Location = New System.Drawing.Point(624, 16)
+        Me.RotateXMinBtn.Name = "RotateXMinBtn"
+        Me.RotateXMinBtn.Size = New System.Drawing.Size(75, 23)
+        Me.RotateXMinBtn.TabIndex = 11
+        Me.RotateXMinBtn.Text = "Rotate X-"
+        Me.RotateXMinBtn.UseVisualStyleBackColor = True
+        '
+        'RotateXPlusBtn
+        '
+        Me.RotateXPlusBtn.Location = New System.Drawing.Point(624, 551)
+        Me.RotateXPlusBtn.Name = "RotateXPlusBtn"
+        Me.RotateXPlusBtn.Size = New System.Drawing.Size(75, 23)
+        Me.RotateXPlusBtn.TabIndex = 12
+        Me.RotateXPlusBtn.Text = "Rotate X+"
+        Me.RotateXPlusBtn.UseVisualStyleBackColor = True
+        '
+        'BackflipBtn
+        '
+        Me.BackflipBtn.Location = New System.Drawing.Point(25, 31)
+        Me.BackflipBtn.Name = "BackflipBtn"
+        Me.BackflipBtn.Size = New System.Drawing.Size(75, 23)
+        Me.BackflipBtn.TabIndex = 13
+        Me.BackflipBtn.Text = "Backflip"
+        Me.BackflipBtn.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'FrontflipBtn
+        '
+        Me.FrontflipBtn.Location = New System.Drawing.Point(106, 31)
+        Me.FrontflipBtn.Name = "FrontflipBtn"
+        Me.FrontflipBtn.Size = New System.Drawing.Size(75, 23)
+        Me.FrontflipBtn.TabIndex = 14
+        Me.FrontflipBtn.Text = "Frontflip"
+        Me.FrontflipBtn.UseVisualStyleBackColor = True
+        '
+        'Timer2
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(962, 576)
+        Me.Controls.Add(Me.FrontflipBtn)
+        Me.Controls.Add(Me.BackflipBtn)
+        Me.Controls.Add(Me.RotateXPlusBtn)
+        Me.Controls.Add(Me.RotateXMinBtn)
+        Me.Controls.Add(Me.DownBtn)
+        Me.Controls.Add(Me.UpBtn)
+        Me.Controls.Add(Me.RotateYMinBtn)
+        Me.Controls.Add(Me.RotateYPlusBtn)
+        Me.Controls.Add(Me.BackwardBtn)
+        Me.Controls.Add(Me.ForwardBtn)
+        Me.Controls.Add(Me.LeftBtn)
+        Me.Controls.Add(Me.RightBtn)
+        Me.Controls.Add(Me.GlControl1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GlControl1 As OpenTK.GLControl
+    Friend WithEvents RightBtn As Button
+    Friend WithEvents LeftBtn As Button
+    Friend WithEvents ForwardBtn As Button
+    Friend WithEvents BackwardBtn As Button
+    Friend WithEvents RotateYPlusBtn As Button
+    Friend WithEvents RotateYMinBtn As Button
+    Friend WithEvents UpBtn As Button
+    Friend WithEvents DownBtn As Button
+    Friend WithEvents RotateXMinBtn As Button
+    Friend WithEvents RotateXPlusBtn As Button
+    Friend WithEvents BackflipBtn As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents FrontflipBtn As Button
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ 3D/Form1.vb
@@ -0,0 +1,214 @@
+﻿Imports OpenTK
+Imports OpenTK.Graphics
+Imports OpenTK.Graphics.OpenGL
+
+Public Class Form1
+    Dim z_position As Double = -50
+    Dim x_position As Double = -40
+    Dim y_position As Double = 0
+    Dim rotateY As Double = 0
+    Dim rotateZ As Double = 0
+    Dim flipmaxPlus As Integer
+    Dim flipmaxMin As Integer
+    Private Sub draw()
+        Dim x As Integer = 10
+        Dim y As Integer = 10
+        Dim z As Integer = 10
+        Dim r As Double = 1
+        Dim g As Double = 0
+        Dim b As Double = 0
+
+        GL.Translate(x_position, y_position, z_position)
+
+        For i As Integer = 1 To 9
+            Select Case i
+                Case 2
+                    x = x + 40
+                    g = 1
+                Case 3
+                    x = x + 40
+                    b = 1
+                Case 4
+                    x = 10
+                    z = z - 50
+                    r = 0
+                Case 5
+                    x = x + 40
+                    g = 0
+                Case 6
+                    x = x + 40
+                    r = 1
+                Case 7
+                    x = 10
+                    z = z - 50
+                    r = 0
+                    g = 1
+                    b = 0
+                Case 8
+                    x = x + 40
+                    r = 1
+                    b = 0.5
+                Case 9
+                    x = x + 40
+                    r = 0.5
+                    g = 0
+                    b = 1
+            End Select
+
+            GL.Begin(BeginMode.LineStrip)
+            'front
+            GL.Color3(r, g, b)
+            GL.Vertex3(x, y, z)
+            GL.Vertex3(x, y - 20, z)
+            GL.Vertex3(x - 20, y - 20, z)
+            GL.Vertex3(x - 20, y, z)
+            GL.Vertex3(x, y, z)
+            GL.End()
+
+            GL.Begin(BeginMode.LineStrip)
+            'back
+            GL.Color3(r, g, b)
+            GL.Vertex3(x, y, z - 20)
+            GL.Vertex3(x, y - 20, z - 20)
+            GL.Vertex3(x - 20, y - 20, z - 20)
+            GL.Vertex3(x - 20, y, z - 20)
+            GL.Vertex3(x, y, z - 20)
+            GL.End()
+
+            GL.Begin(BeginMode.LineStrip)
+            'left
+            GL.Color3(r, g, b)
+            GL.Vertex3(x, y, z - 20)
+            GL.Vertex3(x, y, z)
+            GL.Vertex3(x, y - 20, z)
+            GL.Vertex3(x, y - 20, z - 20)
+            GL.End()
+
+            GL.Begin(BeginMode.LineStrip)
+            'right
+            GL.Color3(r, g, b)
+            GL.Vertex3(x - 20, y - 20, z - 20)
+            GL.Vertex3(x - 20, y - 20, z)
+            GL.Vertex3(x - 20, y, z)
+            GL.Vertex3(x - 20, y, z - 20)
+            GL.End()
+
+        Next i
+
+        'Finally...
+        GraphicsContext.CurrentContext.VSync = True 'Caps frame rate as to not over run GPU
+        GlControl1.SwapBuffers() 'Takes from the 'GL' and puts into control
+    End Sub
+
+    Private Sub GlControl1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GlControl1.Load
+        'Control is loaded, set back color
+        GL.ClearColor(Color.Black)
+    End Sub
+
+    Private Sub GlControl1_Paint(ByVal sender As Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles GlControl1.Paint
+        'First Clear Buffers
+        GL.Clear(ClearBufferMask.ColorBufferBit)
+        GL.Clear(ClearBufferMask.DepthBufferBit)
+
+
+        'Basic Setup for viewing
+        GL.Viewport(0, 0, GlControl1.Width, GlControl1.Height) 'Size of window
+        Dim perspective As Matrix4 = Matrix4.Perspective(45, 1, 1, 10000) 'Setup Perspective
+        GL.MatrixMode(MatrixMode.Projection) 'Load Perspective
+        GL.LoadIdentity()
+        GL.LoadMatrix(perspective)
+        GL.MatrixMode(MatrixMode.Modelview) 'Load Camera
+        GL.LoadIdentity()
+        'GL.Enable(EnableCap.DepthTest) 'Enable correct Z Drawings
+        'GL.DepthFunc(DepthFunction.Less) 'Enable correct Z Drawings
+
+        'Rotating
+        GL.Rotate(rotateZ, 1, 0, 0)
+        GL.Rotate(rotateY, 0, 1, 0)
+
+
+        draw()
+
+
+    End Sub
+
+    Private Sub RightBtn_Click(sender As Object, e As EventArgs) Handles RightBtn.Click
+        GlControl1.Invalidate()
+        x_position -= 4
+    End Sub
+
+    Private Sub LeftBtn_Click(sender As Object, e As EventArgs) Handles LeftBtn.Click
+        GlControl1.Invalidate()
+        x_position += 4
+    End Sub
+
+    Private Sub BackwardBtn_Click(sender As Object, e As EventArgs) Handles BackwardBtn.Click
+        GlControl1.Invalidate()
+        z_position -= 4
+    End Sub
+
+    Private Sub ForwardBtn_Click(sender As Object, e As EventArgs) Handles ForwardBtn.Click
+        GlControl1.Invalidate()
+        z_position += 4
+    End Sub
+
+    Private Sub RotateYPlusBtn_Click(sender As Object, e As EventArgs) Handles RotateYPlusBtn.Click
+        GlControl1.Invalidate()
+        rotateY += 4
+    End Sub
+
+    Private Sub RotateYMinBtn_Click(sender As Object, e As EventArgs) Handles RotateYMinBtn.Click
+        GlControl1.Invalidate()
+        rotateY -= 4
+    End Sub
+
+    Private Sub UpBtn_Click(sender As Object, e As EventArgs) Handles UpBtn.Click
+        GlControl1.Invalidate()
+        y_position -= 4
+    End Sub
+
+    Private Sub DownBtn_Click(sender As Object, e As EventArgs) Handles DownBtn.Click
+        GlControl1.Invalidate()
+        y_position += 4
+    End Sub
+
+    Private Sub RotateXMinBtn_Click(sender As Object, e As EventArgs) Handles RotateXMinBtn.Click
+        GlControl1.Invalidate()
+        rotateZ -= 4
+    End Sub
+
+    Private Sub RotateXPlusBtn_Click(sender As Object, e As EventArgs) Handles RotateXPlusBtn.Click
+        GlControl1.Invalidate()
+        rotateZ += 4
+    End Sub
+
+    Private Sub BackflipBtn_Click(sender As Object, e As EventArgs) Handles BackflipBtn.Click
+        flipmaxMin = rotateZ - 360
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Interval = 10
+        GlControl1.Invalidate()
+        rotateZ -= 10
+        If rotateZ = flipmaxMin Then
+            Timer1.Stop()
+            rotateZ = flipmaxMin
+        End If
+    End Sub
+
+    Private Sub FrontflipBtn_Click(sender As Object, e As EventArgs) Handles FrontflipBtn.Click
+        flipmaxPlus = rotateZ + 360
+        Timer2.Start()
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Timer2.Interval = 10
+        GlControl1.Invalidate()
+        rotateZ += 10
+        If rotateZ = flipmaxPlus Then
+            Timer2.Stop()
+            rotateZ = flipmaxPlus
+        End If
+    End Sub
+End Class
